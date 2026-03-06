702837cd39b0eb47961e9c6cdc9e05d260270352
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2026-03-06 19:45:21 +0100

    Allow the script to continue if the uninstall fails

--- install_opencentauri/uninstall-oc.sh
+++ install_opencentauri/uninstall-oc.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 if [[ -f "/app/uninstall.sh" ]]; then
-    set -e
     chmod a+x /app/uninstall.sh
-    /app/uninstall.sh
+    /app/uninstall.sh || echo "Uninstall failed. Manual cleanup may be required."
 else
     echo "Uninstall script not present. Skipping!"
 fi
